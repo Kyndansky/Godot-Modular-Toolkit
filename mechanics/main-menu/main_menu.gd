a1e16b9eb17c97568e87e563177cc2786caf21a5
Kyndansky <davidericcobene10@gmail.com>
2026-04-30 21:57:18 +0200

    (main menu) added ability to change volume based on a test settings file

--- mechanics/main-menu/main_menu.gd
+++ mechanics/main-menu/main_menu.gd
@@ -2,7 +2,9 @@ extends Control
 
 func _ready() -> void:
 	%FullscreenCheckbox.button_pressed=Settings.is_fullscreen
-
+	%MasterVolumeSlider.value=Settings.master_volume_level
+	%MusicVolumeSlider.value=Settings.music_volume_level
+	%SoundEffectsVolumeSlider.value=Settings.sound_effects_volume_level
 #gets called when one presses the button to close the settings menu
 func _on_close_button_pressed() -> void:
 	$SettingsPanel.visible=false
@@ -17,3 +19,15 @@ func _on_fullscreen_check_box_toggled(toggled_on: bool) -> void:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 	else:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+
+#gets called when one changes the value of the master volume slider
+func _on_master_volume_slider_value_changed(value: float) -> void:
+	Settings.master_volume_level=value
+
+#gets called when one changes the value of the music volume slider
+func _on_music_volume_slider_value_changed(value: float) -> void:
+	Settings.music_volume_level=value
+
+#gets called when one changes the value of the sound effects volume slider
+func _on_sound_effects_volume_slider_value_changed(value: float) -> void:
+	Settings.sound_effects_volume_level=value
--- mechanics/main-menu/settings.gd
+++ mechanics/main-menu/settings.gd
@@ -2,6 +2,8 @@ extends Node
 
 enum Resolution { LOW, MID, HIGH }
 #TODO: add resolution handling
-var volume_level:int=100
+var master_volume_level:float=10.0
+var music_volume_level:float=75.0
+var sound_effects_volume_level:float=50.0
 
-var is_fullscreen=true;
+var is_fullscreen=false;
