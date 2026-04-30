0d77a028a0827481e1140664c852dd2921cb0fce
Kyndansky <davidericcobene10@gmail.com>
2026-04-30 16:51:06 +0200

    (main-menu) Added fullscreen checkbox functionality, added comments

--- mechanics/main-menu/main_menu.gd
+++ mechanics/main-menu/main_menu.gd
@@ -1,7 +1,19 @@
 extends Control
 
+func _ready() -> void:
+	%FullscreenCheckbox.button_pressed=Settings.is_fullscreen
+
+#gets called when one presses the button to close the settings menu
 func _on_close_button_pressed() -> void:
 	$SettingsPanel.visible=false
 
+#gets called when one presses the 'exit' button
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
+
+#handles toggled signal for the checkbox which controls the fullscreen setting
+func _on_fullscreen_check_box_toggled(toggled_on: bool) -> void:
+	if toggled_on==true:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
--- mechanics/main-menu/settings.gd
+++ mechanics/main-menu/settings.gd
@@ -1,4 +1,7 @@
 extends Node
 
 enum Resolution { LOW, MID, HIGH }
+#TODO: add resolution handling
 var volume_level:int=100
+
+var is_fullscreen=true;
