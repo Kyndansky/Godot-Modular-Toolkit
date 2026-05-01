3b5b94bd01b9d69fdc7ef0b7b2b9de19bfe0faed
Kyndansky <davidericcobene10@gmail.com>
2026-05-01 12:42:38 +0200

    (settings-system) Added functions to save/load data for settings.gd to/from a settings.cfg file

--- mechanics/settings-system/settings.gd
+++ mechanics/settings-system/settings.gd
@@ -13,25 +13,25 @@ signal fullscreen_changed(value: bool)
 
 enum Resolution { LOW, MID, HIGH }
 
-var master_volume_level: float = 50.0:
+var master_volume_level: float:
 	set(value):
 		master_volume_level = value
 		AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), linear_to_db(value / 100.0))
 		volume_changed.emit("Master", value)
 		
-var music_volume_level: float = 75.0:
+var music_volume_level: float:
 	set(value):
 		music_volume_level = value
 		AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"), linear_to_db(value / 100.0))
 		volume_changed.emit("Music", value)
 		
-var sound_effects_volume_level:float=50.0:
+var sound_effects_volume_level:float:
 	set(value):
 		sound_effects_volume_level = value
 		AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Sounds"), linear_to_db(value / 100.0))
 		volume_changed.emit("Sounds", value)
 
-var is_fullscreen: bool = false:
+var is_fullscreen: bool:
 	set(value):
 		is_fullscreen = value
 		# Applica direttamente la modalità finestra
@@ -40,3 +40,24 @@ var is_fullscreen: bool = false:
 		else:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 		fullscreen_changed.emit(value)
+
+func save_settings_to_file():
+	var config=ConfigFile.new()
+	config.set_value("Display", "is_fullscreen", is_fullscreen)
+	config.set_value("Audio", "master_volume_level", master_volume_level)
+	config.set_value("Audio", "music_volume_level", music_volume_level)
+	config.set_value("Audio", "sound_effects_volume_level", sound_effects_volume_level)
+	config.save("user://settings.cfg")
+
+
+func load_settings_from_file():
+	var config = ConfigFile.new()
+	var err = config.load("user://settings.cfg")
+	
+	if err != OK:
+		return
+	
+	is_fullscreen=config.get_value("Display", "is_fullscreen")
+	master_volume_level=config.get_value("Audio", "master_volume_level", 50.0)
+	music_volume_level=config.get_value("Audio", "music_volume_level", 75.0)
+	sound_effects_volume_level=config.get_value("Audio", "sound_effects_volume_level", 75.0)
--- mechanics/settings-system/settings_menu.gd
+++ mechanics/settings-system/settings_menu.gd
@@ -2,6 +2,7 @@ extends PanelContainer
 
 #initializing button/sliders values based on settings
 func _ready() -> void:
+	Settings.load_settings_from_file() #do this when you want to load settings
 	%FullscreenCheckbox.button_pressed=Settings.is_fullscreen
 	%MasterVolumeSlider.value=Settings.master_volume_level
 	%MusicVolumeSlider.value=Settings.music_volume_level
@@ -22,4 +23,5 @@ func _on_sound_effects_volume_slider_value_changed(value: float) -> void:
 
 
 func _on_close_button_pressed() -> void:
+	Settings.save_settings_to_file() #do this every now and then or when exiting the game
 	self.visible=false
